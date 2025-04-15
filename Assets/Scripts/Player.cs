c5c22dbbf00b01dc58a52a9a8b3b930857d312cb
shgmlrkd <jack342@naver.com>
2025-04-15 21:54:26 +0900

    플레이어 애니메이션 끝, 가상 카메라 쌤 도와줘요

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.InputSystem.Controls;
 using TMPro;
 
 public class Player : MonoBehaviour
@@ -31,10 +32,7 @@ public class Player : MonoBehaviour
     private float _playerMoveSpeed = 6.0f;
     private float _playerRotateSpeed = 12.0f;
 
-    /*private float walkSpeed = 6.0f;
-    private float runSpeed = 12.0f;
-    private float rotateSpeed = 12.0f;*/
-    //Animator animator;
+    private bool _isRunning = false;
 
     private void Awake()
     {
@@ -54,7 +52,6 @@ public class Player : MonoBehaviour
             healthSlider.value = currentHealth;
         }
         anim = GetComponent<Animator>();
-        healthSlider.AddComponent<Slider>();
     }
 
     void Update()
@@ -63,57 +60,23 @@ public class Player : MonoBehaviour
 
         if (inputDir.sqrMagnitude > 0)
         {
-            //anim.SetFloat("Run", inputDir.sqrMagnitude);
+            _isRunning = true;
             transform.Translate(inputDir.normalized * _playerMoveSpeed * Time.deltaTime, Space.World); //position += inputDir * playerMoveSpeed * Time.deltaTime;
 
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(inputDir), Time.deltaTime * _playerRotateSpeed);
         }
+        else
+        {
+            _isRunning = false;
+        }
+
+        anim.SetBool("IsRunning", _isRunning);
 
         if (Input.GetKeyDown(KeyCode.H))
         {
             TakeDamage(10f);
         }
-        //anim.SetFloat("Idle", inputDir.sqrMagnitude);
-        // 상태 전이
-        /*  if (inputDir.sqrMagnitude == 0)
-          {
-              ChangeState(State.Idle);
-              animator.SetTrigger("Walk");
-          }
-          else if (Input.GetKey(KeyCode.LeftShift))
-          {
-              ChangeState(State.Running);
-              animator.SetTrigger("Run");
-          }
-          else
-          {
-              ChangeState(State.Walking);
-              animator.SetTrigger("Walk");
-
-          }*/
-
-        //Move(inputDir);
     }
-    /*private void Move(Vector3 dir)
-    {
-        if (dir.sqrMagnitude == 0) return;
-
-        float moveSpeed = (currentState == State.Running) ? runSpeed : walkSpeed;
-        dir.Normalize();
-
-
-        transform.Translate(dir * moveSpeed * Time.deltaTime, Space.World);
-
-        Quaternion targetRot = Quaternion.LookRotation(dir);
-        transform.rotation = Quaternion.Lerp(transform.rotation, targetRot, Time.deltaTime * rotateSpeed);
-    }
-
-    void ChangeState(State newState)
-    {
-        if (currentState == newState) return;
-
-        currentState = newState;
-    }*/
     public void TakeDamage(float amount)
     {
         currentHealth -= amount;
